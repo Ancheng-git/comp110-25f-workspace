3e70e0041ad0fc7238bda7941bb0ea264d310b1c
ancheng <ancheng@ad.unc.edu>
2025-08-20 11:55:47 -0400

    Finished exercise 00

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,11 @@
+"""My first exercise in COMP110!"""
+
+__author__ = "730922305"
+
+
+def greet(name: str) -> str:
+    return "Hello," + name + "!"
+
+
+if __name__ == "__main__":
+    print(greet(name=input("What is your name?")))
