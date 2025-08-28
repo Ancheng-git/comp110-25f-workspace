3974a0adcaf93070a91fe780cb59f60a8a5cf12e
ancheng <ancheng@ad.unc.edu>
2025-08-28 14:40:59 -0400

    ex01

---
+++ exercises/ex01_tea_party.py
@@ -0,0 +1,35 @@
+"""Tea Party Program"""
+
+__author__: str = "730922305"
+
+
+def main_planner(guests: int) -> None:
+    """list everything needed"""
+    print("A Cozy Tea Party for " + str(guests) + " People!")
+    print("Tea Bags: " + str(tea_bags(people=guests)))
+    print("Treats: " + str(treats(people=guests)))
+    print(
+        "Cost: $"
+        + str(
+            cost(tea_count=tea_bags(people=guests), treat_count=treats(people=guests))
+        )
+    )
+
+
+def tea_bags(people: int) -> int:
+    """calculate the tea bags needed"""
+    return people * 2
+
+
+def treats(people: int) -> int:
+    """calculate the treats needed"""
+    return int(1.5 * tea_bags(people=people))
+
+
+def cost(tea_count: int, treat_count: int) -> float:
+    """calculate the total cost"""
+    return 0.5 * tea_count + 0.75 * treat_count
+
+
+if __name__ == "__main__":
+    main_planner(guests=int(input("How many guests are attending your tea party?")))
